b26c5bed5f3ba8cec0c589eb27bf5e4e8264dd9b
Joschua Conrad <32811308+Joschua-Conrad@users.noreply.github.com>
2024-10-31 08:25:29 +0100

    More docstrings, more exception in plot and actually using label in plot now

--- psumsim/plot.py
+++ psumsim/plot.py
@@ -1,9 +1,84 @@
+"""Basic plot functionality.
+
+Plotting is done using `matplotlib`, which is imported contextual: the import
+takes some time and we don't want the commandline to halt every time it is
+called.
+"""
+
 import numpy as np
 from .array import normalizeAxes
 from .hist import getHistValues, unpackHist, packStatistic
 
 
 def plotHist(hist, histaxis, stataxis, refbincount, axorfigname, xlabel, ylabel, label):
+	"""Plot a histogram.
+
+	Parameters
+	----------
+	hist : `numpy.ndarray`
+		Some array. A return value of *dostatisticdummy* can get its statisitc
+		axis resolved to probabilities. A return value of *dostatistic* is first
+		unpacked to `bool` histogram and then also resolved to probabilities.
+		So outputs of all simulation modes (see `statstoc`) are supported.
+		
+	histaxis : `int`
+		The axis along which one finds the different histogram bins.
+		
+	stataxis : `int`, `None`
+		If given, `packStatistic` is used along this axis to turn actual
+		statistic computation results into value probabilities. See `statstoc`.
+		
+	refbincount : `int`, `None`
+		The *bincount* is the number of bins along the x-axis. By default,
+		the length of *histaxis* is used. But results of *dostatistic*
+		(see `statstoc`) return a length-1 *histaxis*, are unpacked using
+		`unpackHist` and that needs a *bincount*. That value is taken from this
+		parameter. If *histaxis* has a larger length, this parameter is
+		ignored.
+		
+	axorfigname : `matplotlib.axes.Axes`, `str`
+		The object to plot into. Either pass an existing axes object or the
+		name of a new `matplotlib.figure.Figure`.
+		
+	xlabel : `str`, `None`
+		If given, this is the new x-axis label.
+		
+	ylabel : `str`, `None`
+		If given, this is the new y-axis label.
+		
+	label : `str`, `None`
+		If given, this is the legend text of the created plot. Later use
+		`matplotlib.axes.Axes.legend` to create the legend.
+		
+	Raises
+	------
+	`ValueError`
+		If *histaxis* and *stataxis* are the same, or if *refbincount* was
+		not passed, but it needed.
+
+	Returns
+	-------
+	fig : `matplotlib.figure.Figure`
+		Figure object.
+		
+	ax : `matplotlib.axes.Axes`
+		Axes object.
+		
+	hist : `numpy.ndarray`
+		The bar heights as plotted after possibly using `unpackHist` and
+		`packStatistic`.
+		
+	histplot : `matplotlib.container.BarContainer`
+		Returned by `matplotlib.axes.Axes.bar`.
+		
+	bincount : `int`
+		The now used *bincount*, which is ready for being used on a next plot.
+		Often, one would like to plot *dostochastic* and *dostatistic* in
+		one figure. Plot the stochastic data then first, remember this returned
+		*bincount*, and use it as *refbincount* for plotting statistic data.
+		The x-axis lengths are the same then.
+
+	"""
 	
 	#Contextual import, because this one takes long
 	from matplotlib import pyplot as plt
@@ -12,12 +87,20 @@ def plotHist(hist, histaxis, stataxis, refbincount, axorfigname, xlabel, ylabel,
 	#Stataxis can be none meaning "there is no stat axis"
 	if stataxis is not None:
 		stataxis, = normalizeAxes(axes=stataxis, referencendim=hist.ndim)
+		
+		if histaxis == stataxis:
+			raise IndexError(
+					f"histaxis {histaxis} and stataxis {stataxis} cannot be "
+					f"the same.",
+					histaxis,
+					stataxis,
+			)
 	
 	#User can give axes to plot into or just a tring meaning, create a figure
 	#with this na,e
 	if isinstance(axorfigname, str):
 		fig = plt.figure(num=axorfigname)
-		ax = fig.add_subplot(111)
+		ax = fig.subplots()
 	else:
 		ax = axorfigname
 		fig = ax.get_figure()
@@ -25,14 +108,16 @@ def plotHist(hist, histaxis, stataxis, refbincount, axorfigname, xlabel, ylabel,
 	#Get bincount from values to plot
 	bincount = hist.shape[histaxis]
 	
-	#If refbincount is None, use just found bincount
-	if refbincount is None:
-		refbincount = bincount
-	
 	#If we have length 1 along hist axis, unpack. Use bincount maybe known
 	#from previous call. That way, one can plot uint results with
 	#known bincount or statistic results and let us find the bincount.
 	if bincount == 1:
+		if refbincount is None:
+			raise ValueError(
+					"Plotting a histogram, which needs unpacking. "
+					"But *refbincount* was not passed."
+			)
+		
 		hist = unpackHist(hist, bincount=refbincount, axis=histaxis)
 		#This increases the bincount from 1 to some actual hist
 		bincount = int(refbincount)
@@ -67,6 +152,7 @@ def plotHist(hist, histaxis, stataxis, refbincount, axorfigname, xlabel, ylabel,
 			width=1.05,
 			align="center",
 			bottom=0,
+			label=label,
 	)
 	
 	if xlabel is not None:
