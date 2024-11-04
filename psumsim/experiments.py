Joschua-Conrad/PSumSim

commit 22c50dc82b9071531e32cb63df5d156d7a8c048c

Fixed bug in doSingleRun, which lead to miorly off OCC value

psumsim/experiments.py
@@ -695,6 +695,8 @@ def doSingleRun(rundescription, sqnrreferencereturn):
 	)
 	processedcliplimitcases = list()
 	for cliplimit, levels in cliplimitcases:
+		#All levels are a histlen, but the OCC expects a bincount.
+		levels = histlenToBincount(histlen=levels)
 		if cliplimit == "occ":
 			cliplimit = optimumClippingCriterion(
 					levels=levels,

psumsim/simulate.py
@@ -1165,7 +1165,8 @@ def quantizeClipScaleValues(
 		Parameterizes quantization.
 		If positive, is used as described in `quantization` to merge bins by
 		dividing and rounding. If negative, describes the *histlen* (so the
-		maximum magnitude) of the quantized histogram. Values on range *(-1;1)*
+		maximum magnitude) of the quantized histogram. See `dataformat`.
+		Values on range *(-1;1)*
 		or larger than the old histogram length
 		would leave us with less than *3* bins or would have no effect.
 		They raise an exception.
@@ -4057,7 +4058,7 @@ def optimumClippingCriterion(levels, abstol=1e-6, maxiter=100):
 	----------
 	levels : `int`
 		The number of levels in the quantizer following clipping. [OCC]_ uses
-		a bitwidth *B*. This is a *bincount*.
+		a bitwidth *B*. This is a *bincount*. See `dataformat`
 		
 	abstol : `float`, optional
 		The absolute tolerance t be achieved in the iterative computation.

commit f8d406c53ec0b1da9726886fbd8a51cbf0633ca9

Fixed bug introduced in 22c50dc82b9071531e32cb63df5d156d7a8c048c

psumsim/experiments.py
@@ -695,9 +695,9 @@ def doSingleRun(rundescription, sqnrreferencereturn):
 	)
 	processedcliplimitcases = list()
 	for cliplimit, levels in cliplimitcases:
-		#All levels are a histlen, but the OCC expects a bincount.
-		levels = histlenToBincount(histlen=levels)
 		if cliplimit == "occ":
+			#All levels are a histlen, but the OCC expects a bincount.
+			levels = histlenToBincount(histlen=levels)
 			cliplimit = optimumClippingCriterion(
 					levels=levels,
 			)